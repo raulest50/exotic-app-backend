b6acd227fcfb519de25a54786883050e50d48f67
ornonsu.linux <raulest50@gmail.com>
2026-08-18 20:00:46 -0500

    compilation minor fix

--- src/main/java/exotic/app/planta/service/produccion/SeguimientoOrdenAreaService.java
+++ src/main/java/exotic/app/planta/service/produccion/SeguimientoOrdenAreaService.java
@@ -56,6 +56,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
